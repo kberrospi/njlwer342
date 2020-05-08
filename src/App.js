3ff25c12cfb29a6b0ca02dd0e7bd3899288cb2da
Keivin <keivinbr13@gmail.com>
2020-05-08 12:17:11 -0500

    Tareas

--- src/App.js
+++ src/App.js
@@ -3,18 +3,57 @@ import React, { Component } from 'react';
 /// Modifica el componente para que se puedan agregar tareas
 
 class App extends Component {
+
+  state= {
+    tareas: [
+      "Sacar la ropa", 
+      "Hacer la cama", 
+      "Leer un rato" 
+    ],
+
+    val: '',
+    
+  }
+
+  chang = (e) =>{
+    e.preventDefault();
+    this.setState({
+      val: e.target.value,
+    });
+  }
+ 
+  addWork = (e) =>{
+   
+    if(e.key === 'Enter'){
+      e.preventDefault();
+
+      this.setState({
+        tareas: this.state.tareas.concat(e.target.value),
+        val: ''
+      });
+
+      
+    }
+  
+  }
+
+  
+
+
   render() {
     return (
       <div className="wrapper">
         <div className="list">
           <h3>Por hacer:</h3>
           <ul className="todo">
-            <li>Sacar la ropa</li>
-            <li>Hacer la cama</li>
-            <li>Leer un rato</li>
+            {
+              this.state.tareas.map((e, i)=>
+              <li key={i}>{e}</li>
+              ) 
+            }
           </ul>
            <form>
-             <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" />
+             <input type="text" value={this.state.val} onChange={this.chang} onKeyPress={this.addWork}  id="new-task" placeholder="Ingresa una tarea y oprime Enter"  />
            </form>
         </div>
       </div>
